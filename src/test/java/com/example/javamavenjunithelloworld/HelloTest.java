5ec3c56c05ec9f0589e5430e03dd2310f0091ce7
magzhan.karasayev <fallen.s4e@gmail.com>
2017-12-18 14:38:31 +0600

    master: Upd: bug introduced

--- src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
@@ -41,13 +41,13 @@ public class HelloTest {
         assertThat(os.toString(), is(equalTo(goal)));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = NullPointerException.class)
     public void testIllegalArgumentForHello21() {
         Hello hi = new Hello();
         hi.setTimes(Hello.MAXIMUM_AMOUNT_OF_TIMES + 1);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = NullPointerException.class)
     public void testIllegalArgumentForHelloNegative() {
         Hello hi = new Hello();
         hi.setTimes(-1);
